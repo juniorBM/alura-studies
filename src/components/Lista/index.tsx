1ec8c178d6942ef33031674d249ffe6bbaccef60
juniorBM <juniorbev.mendonca@gmail.com>
2023-03-08 08:58:26 -0300

    init project

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Item from "./Item";
+import style from "./Lista.module.scss";
+
+function Lista() {
+  const [tarefas, setTarefas] = useState([
+    {
+      tarefa: "React",
+      tempo: "02:00:00",
+    },
+    {
+      tarefa: "JavaScript",
+      tempo: "01:00:00",
+    },
+    {
+      tarefa: "TypeScript",
+      tempo: "03:00:00",
+    },
+  ]);
+
+  return (
+    <aside className={style.listaTarefas}>
+      <h2
+        onClick={() => {
+          setTarefas([...tarefas, { tarefa: "Estudar state", tempo: "01:00" }]);
+        }}
+      >
+        Estudos do Dia
+      </h2>
+      <ul>
+        {tarefas.map((item, index) => (
+          <Item key={index} {...item} />
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+export default Lista;
